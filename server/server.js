e26781fa1a6e6778e53a1ef3671ae010ed7902b1
Oskar Pester <oskar.pester2@gmail.com>
2022-01-13 17:20:43 +0100

    added monitoirn implementation

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api')
 const fs = require('fs')
+const axios = require('axios')
 
 
 // Create a bot that uses 'polling' to fetch new updates
@@ -16,3 +17,17 @@ for (const file of eventFiles) {
     const event = require(`./events/${file}`);
     bot.on(event.name, (...args) => event.execute(bot, ...args));
 }
+
+
+console.log(`${process.env.PROJECT_NAME} is running `)
+axios({
+    method:"post",
+    url: "http://localhost:42015/app/register",
+    data:{
+        project_name: process.env.PROJECT_NAME,
+        project_description: process.env.PROJECT_DESCRIPTION,
+        project_port: process.env.PORT
+    }
+})
+    .then(response => console.log(response.data.data))
+    .catch(error => console.error(error.toString()))
